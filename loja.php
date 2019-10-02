92fea42003daaaeb86b0daa98d72bbae401c5646
Anaê Soares <anae08@gmail.com>
2019-10-02 11:49:21 -0300

    inserido sub menu com php

--- loja.php
+++ loja.php
@@ -2,10 +2,11 @@
 <?php 
     $usuario = ["nome"=>"Anaê"];
     $produtos =[
-        ["nome"=>"Curso HTML e CSS", "preco"=>1200.00, "duracao"=>"5 meses"],
-        ["nome"=>"PHP", "preco"=>1000.00, "duracao"=>"8 meses"],
-        ["nome"=>"Javascript", "preco"=>1500.00, "duracao"=>"12 meses"],
+        ["nome"=>"Curso HTML e CSS", "preco"=>1200.00, "duracao"=>"5 meses","img"=>"images/html.svg"],
+        ["nome"=>"PHP", "preco"=>1000.00, "duracao"=>"8 meses","img"=>"images/php.png"],
+        ["nome"=>"Javascript", "preco"=>1500.00, "duracao"=>"12 meses","img"=>"images/js.png"],
     ];
+    $categorias=["Cursos","Palestras", "Artigos","Livros","Ebook"];
 ?>
 <html lang="pt-BR">
 <head>
@@ -47,13 +48,41 @@
                     </li>
                 </ul>
                 <?php } ?>
+            </div>
+        </nav>
+    </header>
+    
+    <div class="container-fluid">
+    <hr style="background-color:orange">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <?php 
+                    if(isset($categorias) && $categorias != []){
+                ?>
+                <?php 
+                    foreach($categorias as $categoria){
+                ?>
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><?php echo $categoria;?></a>
+                        </li>
+                    </ul>
+                <?php } ?>
                 <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
               </form>
+                    <?php }else{
+                        echo "Não possuímos categorias";
+                    }
+                    ?>
             </div>
         </nav>
-    </header>
+                
+    </div>
     <main>  
         <section class="container d-flex flex-direction-row justify-content-around align-item-center mt-5">
         <?php
@@ -64,7 +93,7 @@
         ?>
             <div class="card text-center" style="width: 18rem;">
                 <h3><?php echo $produto["nome"];?></h3>
-                    <img src="images/html.svg" class="card-img-top" alt="logo do html">
+                    <img src=<?php echo $produto['img'];?> class="card-img-top" alt="logo do html">
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $produto["nome"];?></h5>
                                 <p class="card-text"><?php echo $produto["preco"];?></p>
@@ -72,7 +101,7 @@
                                 <a href="#" class="btn btn-primary">Comprar</a>    
                         </div>    
             </div>
-        <?php } ?>
+        <?php } ?> <!--Fechando foreach-->
         <?php }else{ //caso não tenha produtos cadastrados, mostrar mensagem abaixo
             echo "Não possuímos produtos no momento";
         }

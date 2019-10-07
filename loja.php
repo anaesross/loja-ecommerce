e556dc460649660e4c932be769f688471e65b1a7
Anaê Soares <anae08@gmail.com>
2019-10-07 10:53:43 -0300

    Adicionado arquivos da aula que faltei e retirado o menu do loja inserido com include

--- loja.php
+++ loja.php
@@ -18,41 +18,7 @@
     <title>Loja Virtual</title>
 </head>
 <body>
-<!--MENU-->
-    <header class="container-fluid">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <img style="width: 10%;padding:10px" src="images/logo.png"/>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                <?php
-                    if(isset($usuario) && $usuario != ""){
-                ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cursos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Olá <?php echo $usuario['nome'];?></a>
-                    </li>
-                <?php }else{?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cursos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
-                    </li>
-                    </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Cadastrar</a>
-                    </li>
-                </ul>
-                <?php } ?>
-            </div>
-        </nav>
-    </header>
-<!--FIM MENU-->
+    <?php include("menu.php") ?>
 <!--SUB MENU-->
     <div class="container-fluid">
     <hr style="background-color:orange">
---
+++ menu.php
@@ -0,0 +1,36 @@
+
+<!--MENU-->
+<header class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <img style="width: 10%;padding:10px" src="images/logo.png"/>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <?php
+                if(isset($usuario) && $usuario != ""){
+            ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cursos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Olá <?php echo $usuario['nome'];?></a>
+                </li>
+            <?php }else{?>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cursos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Cadastrar</a>
+                </li>
+            </ul>
+            <?php } ?>
+        </div>
+    </nav>
+</header>
+<!--FIM MENU-->
